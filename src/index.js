1e5d3ea491e7d372b577c2708672fee8b4972834
Maxwell Martin <maxmart97@gmail.com>
2020-12-06 16:29:09 -0600

    Create board with loop instead of hardcoding it - Thanks  https://stackoverflow.com/a/45785715/12700619

--- src/index.js
+++ src/index.js
@@ -20,26 +20,20 @@ class Board extends React.Component {
         );
     }
 
+    createBoard() {
+        let rows = []
+        for (let i = 0; i < 3; i++) {
+            let cols = [];
+            for (let j = 0; j < 3; j++) {
+                cols.push(this.renderSquare(3 * i + j));
+            }
+            rows.push(<div className="board-row">{cols}</div>);
+        }
+        return rows;
+    }
+
     render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+        return (<div>{this.createBoard()}</div>);
     }
 }
 
